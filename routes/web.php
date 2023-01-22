9acedbaa0aca830dfb61c5d280f509d9a0adf2a5
Lis Shporta <lis.shporta@gmail.com>
2023-01-22 12:17:24 +0100

    Updated Main Page

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/admin');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
